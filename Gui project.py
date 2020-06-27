38a5ec5bad66d713d59410fafd10d314687e01cd
safwanvk <safwanvalakundil@gmail.com>
2020-06-27 14:37:23 +0530

    Improve gui

--- Gui project.py
+++ Gui project.py
@@ -3,34 +3,30 @@ from tkinter import *
 window = Tk()
 window.geometry('500x500')
 window.title('calculator')
-window.configure(bg='white')
+window.configure(bg='black')
 
 
+def press():
+    print('hello')
 
 
-def hello():
-    print('button clicked')
-
-
-btn7 = Button(window, text='7', command=hello)
-btn8 = Button(window, text='8', command=hello)
-btn9 = Button(window, text='9', command=hello)
-btn_div = Button(window, text='/', command=hello)
-btn4 = Button(window, text='4', command=hello)
-btn5 = Button(window, text='5', command=hello)
-btn6 = Button(window, text='6', command=hello)
-btn_mul = Button(window, text='x', command=hello)
-btn1 = Button(window, text='1', command=hello)
-btn2 = Button(window, text='2', command=hello)
-btn3 = Button(window, text='3', command=hello)
-btn_min = Button(window, text='-', command=hello)
-btn0 = Button(window, text='0', command=hello)
-btn_dot = Button(window, text='.', command=hello)
-btn_mod = Button(window, text='%', command=hello)
-btn_plus = Button(window, text='+', command=hello)
-btn_res = Button(window, text='=', command=hello)
-btn_clr = Button(window, text='C', command=hello)
-
+btn7 = Button(window, text='7', bg='grey', command=lambda: press(7), height=1, width=7)
+btn8 = Button(window, text='8', command=lambda: press(8), height=1, width=7, bg='grey')
+btn9 = Button(window, text='9', command=lambda: press(9), height=1, width=7, bg='grey')
+btn_div = Button(window, text='/', command=lambda: press('/'), height=1, width=7, bg='grey')
+btn4 = Button(window, text='4', command=lambda: press(4), height=1, width=7, bg='grey')
+btn5 = Button(window, text='5', command=lambda: press(5), height=1, width=7, bg='grey')
+btn6 = Button(window, text='6', command=lambda: press(6), height=1, width=7, bg='grey')
+btn_mul = Button(window, text='x', command=lambda: press('x'), height=1, width=7, bg='grey')
+btn1 = Button(window, text='1', command=lambda: press(1), height=1, width=7, bg='grey')
+btn2 = Button(window, text='2', command=lambda: press(2), height=1, width=7, bg='grey')
+btn3 = Button(window, text='3', command=lambda: press(3), height=1, width=7, bg='grey')
+btn_min = Button(window, text='-', command=lambda: press('-'), height=1, width=7, bg='grey')
+btn0 = Button(window, text='0', command=lambda: press(0), height=1, width=7, bg='grey')
+btn_des = Button(window, text='%', command=lambda: press('.'), height=1, width=7, bg='grey')
+btn_plus = Button(window, text='+', command=lambda: press('+'), height=1, width=7, bg='grey')
+btn_res = Button(window, text='=', command=lambda: press('='), height=1, width=7, bg='blue')
+btn_clr = Button(window, text='C', command=lambda: press('C'), height=1, width=7, bg='grey')
 
 btn7.grid(row=0, column=0)
 btn8.grid(row=0, column=1)
@@ -45,11 +41,12 @@ btn2.grid(row=2, column=1)
 btn3.grid(row=2, column=2)
 btn_min.grid(row=2, column=3)
 btn0.grid(row=3, column=0)
-btn_dot.grid(row=3, column=1)
-btn_mod.grid(row=3, column=2)
-btn_plus.grid(row=3, column=3)
-btn_res.grid(row=0, column=4)
+btn_des.grid(row=3, column=1)
+btn_plus.grid(row=3, column=2)
+btn_res.grid(row=3, column=3)
 btn_clr.grid(row=0, column=5)
 
+
+expressi= Entry(window, textvariable='enter')
 
 window.mainloop()
